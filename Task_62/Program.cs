6a1c83d07d3cc5bf8268c667352d7a6a8319e656
AnnBerinceva <berinceva92@bk.ru>
2022-12-05 18:52:36 +0500

    Task_62

---
+++ Task_62/Program.cs
@@ -0,0 +1,37 @@
+﻿int a = 4;
+int[,] sqareMatrix = new int[a, a];
+
+int temp = 1;
+int b = 0;
+int c = 0;
+
+while (temp <= sqareMatrix.GetLength(0) * sqareMatrix.GetLength(1))
+{
+  sqareMatrix[b, c] = temp;
+  temp++;
+  if (b <= c + 1 && b + c < sqareMatrix.GetLength(1) - 1)
+    c++;
+  else if (b < c && b + c >= sqareMatrix.GetLength(0) - 1)
+    b++;
+  else if (b >= c && b + c > sqareMatrix.GetLength(1) - 1)
+    c--;
+  else
+    b--;
+}
+
+WriteArray(sqareMatrix);
+
+void WriteArray (int[,] array)
+{
+  for (int b = 0; b < array.GetLength(0); b++)
+  {
+    for (int c = 0; c < array.GetLength(1); c++)
+    {
+      if (array[b,c] / 10 <= 0)
+      Console.Write($" {array[b,c]} ");
+
+      else Console.Write($"{array[b,c]} ");
+    }
+    Console.WriteLine();
+  }
+}
